1205cdfde9bcef10f129192f9ac8aad647282a09
danielrossyamitrano <32693604Zd>
2020-12-03 11:50:39 -0300

    Completados los ejercicios del día 3 + Aunque necesité ayuda de Reddit para resoleverlos.

---
+++ 2020/03/2020-3.py
@@ -0,0 +1,34 @@
+file = open('input.txt').readlines()
+
+rows = []
+for line in file:
+    line = line.rstrip('\n')
+    rows.append([i for i in line])
+
+slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+ex1 = 0
+trees_ex2 = []
+for i, slope in enumerate(slopes):
+    x, y = -slope[0], -slope[1]
+    trees_ex2.append(0)
+    while True:
+        x += slope[0]
+        if x >= 31:
+            x = x - 31
+        y += slope[1]
+        if y >= 323:
+            break
+
+        if rows[y][x] == '#':  # suggested by Reddit user u/MiataCory
+            if i == 1:
+                ex1 += 1
+            trees_ex2[i] += 1
+
+
+print(ex1)
+
+ex2 = trees_ex2[0]
+for i in trees_ex2[1:]:
+    ex2 *= i
+
+print(ex2)
