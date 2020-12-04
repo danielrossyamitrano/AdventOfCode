eaa0abdf25781c9bd4327e2c871c8f446b3754a7
danielrossyamitrano <32693604Zd>
2020-12-04 15:53:11 -0300

    Invertidos los slices porque estaban al revés. Aunque encontraba la respuesta igual :S

--- 2020/01/2020-1.py
+++ 2020/01/2020-1.py
@@ -3,13 +3,13 @@ numbers = [int(number.rstrip('\n')) for number in open('input.txt')]
 result_ex1 = 0
 result_ex2 = 0
 for i, number in enumerate(numbers):
-    for other in numbers[:i+1]:  # looping by index as suggested by GitHub user einacio
+    for other in numbers[i+1:]:  # looping by index as suggested by GitHub user einacio
         if result_ex1 == 0:  # inverted "in clause" as suggested by GitHub user einacio
             if number + other == 2020:
                 result_ex1 = number * other
                 break
         if result_ex2 == 0:
-            for another in numbers[:i+2]:
+            for another in numbers[i+2:]:
                 if number + other + another == 2020:
                     result_ex2 = number*other*another
                     break
