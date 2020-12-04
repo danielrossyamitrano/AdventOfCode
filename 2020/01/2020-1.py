5aaadf36836c7aa09509b803b67d52a55dae8050
danielrossyamitrano <32693604Zd>
2020-12-04 15:23:35 -0300

    Hechas algunas optimizaciones sugeridas por Ein al código primer día.

--- 2020/01/2020-1.py
+++ 2020/01/2020-1.py
@@ -1,22 +1,18 @@
 numbers = [int(number.rstrip('\n')) for number in open('input.txt')]
 
-cheked = []
 result_ex1 = 0
 result_ex2 = 0
-for number in numbers:
-    if number not in cheked:
-        cheked.append(number)
-    for other in numbers:
-        if other not in cheked and result_ex1 == 0:
+for i, number in enumerate(numbers):
+    for other in numbers[:i+1]:  # looping by index as suggested by GitHub user einacio
+        if result_ex1 == 0:  # inverted "in clause" as suggested by GitHub user einacio
             if number + other == 2020:
                 result_ex1 = number * other
                 break
         if result_ex2 == 0:
-            for another in numbers:
-                if other not in cheked or another not in cheked:
-                    if number + other + another == 2020:
-                        result_ex2 = number*other*another
-                        break
+            for another in numbers[:i+2]:
+                if number + other + another == 2020:
+                    result_ex2 = number*other*another
+                    break
     if result_ex1 and result_ex2:
         break
 
