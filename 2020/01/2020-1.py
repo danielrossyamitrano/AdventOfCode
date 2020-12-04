f846eccffd985dd49a4cc3b91f8b9dc47055a16a
danielrossyamitrano <32693604Zd>
2020-12-04 13:54:18 -0300

    Hecho más eficiente el algoritmo del día 1. + Ahora imprime los dos resultados y lo hace mucho más rápido.

--- 2020/01/2020-1.py
+++ 2020/01/2020-1.py
@@ -1,17 +1,24 @@
-from os import getcwd
-from os.path import join
-
-ruta = join(getcwd(), 'input.txt')
-file = open(ruta)
-
-numbers = [int(number.rstrip('\n')) for number in file]
+numbers = [int(number.rstrip('\n')) for number in open('input.txt')]
 
 cheked = []
+result_ex1 = 0
+result_ex2 = 0
 for number in numbers:
     if number not in cheked:
         cheked.append(number)
     for other in numbers:
-        for another in numbers:
-            if other not in cheked or another not in cheked:
-                if number + other + another == 2020:
-                    print(number*other*another)
+        if other not in cheked and result_ex1 == 0:
+            if number + other == 2020:
+                result_ex1 = number * other
+                break
+        if result_ex2 == 0:
+            for another in numbers:
+                if other not in cheked or another not in cheked:
+                    if number + other + another == 2020:
+                        result_ex2 = number*other*another
+                        break
+    if result_ex1 and result_ex2:
+        break
+
+print('result ex 1: ', result_ex1)
+print('result ex 1: ', result_ex2)
