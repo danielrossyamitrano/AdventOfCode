19d947f606fb458cb8e589ddecd904f511e55947
danielrossyamitrano <32693604Zd>
2020-12-05 11:22:15 -0300

    Completados los ejercicios del día 5 * Actualizada la función en main.py * agregada la función abrir_json a main.py y un archivo ignorado   "day.json" para no estar modificando main.py más de la cuenta.

---
+++ 2020/05/solutions.py
@@ -0,0 +1,55 @@
+from math import floor, ceil
+from os import getcwd, path
+
+high = []
+result_ex1 = 0
+seats = []
+
+with open(path.join(getcwd(), '05', 'input.txt'), 'rt') as file:
+    for line in file.readlines():
+        sample = line.rstrip('\n')
+
+        mi, ma = 0, 127
+        row = 0
+        for char in sample[0:7]:
+            if char == 'F':
+                ma = floor((ma-mi)/2+mi)
+            elif char == 'B':
+                mi = ceil(ma/2)+ceil(mi/2)
+
+            row = mi if char == 'F' else ma if char == 'B' else 0
+
+        mi, ma = 0, 7
+        column = 0
+        for char in sample[-3:]:
+            if char == 'L':
+                ma = floor((ma - mi) / 2 + mi)
+            elif char == 'R':
+                mi = ceil(ma / 2) + ceil(mi / 2)
+
+            column = mi if char == 'L' else ma if char == 'R' else 0
+
+        seat_id = row*8+column
+        seats.append(seat_id)
+        high.append(seat_id)
+        result_ex1 = max(high)
+
+
+print('result ex1:', result_ex1)
+
+result_ex2 = 0
+seats.sort()
+asientos = []
+for row in range(128):
+    for column in range(8):
+        asientos.append(row*8+column)
+
+asientos.sort()
+
+for silla in asientos:
+    x = silla % 8
+    y = silla // 128
+    if silla not in seats and y not in (0, 7):
+        result_ex2 = silla
+
+print('result ex2:', result_ex2)
--- 2020/main.py
+++ 2020/main.py
@@ -1,14 +1,18 @@
-from importlib.machinery import SourceFileLoader
+from importlib import import_module
 from time import perf_counter
+import json
 
 
 def raw_load_module(ruta: str):
-    # noinspection PyArgumentList
-    _module = SourceFileLoader("module.name", ruta).load_module()
-    # deprecated, but I don't understand how to use the new method.
-    return _module
+    name = ruta.replace('/', '.')
+    return import_module(name, ruta)
+
+
+def abrir_json(ruta, encoding='utf-8'):
+    with open(ruta, 'r', encoding=encoding) as file:
+        return json.load(file)
 
 
 perf_counter()
-module = raw_load_module('04/solutions.py')
+module = raw_load_module('2020/'+abrir_json('day.json')+'/solutions')
 print('\nPerformed in {} seconds'.format(perf_counter()))
